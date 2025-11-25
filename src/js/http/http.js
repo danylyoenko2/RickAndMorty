593b739c09c330d34e07c21fb6280b8f162a6065
Danil <lockmaster19@gmail.com>
2025-11-25 20:42:27 +0200

    upd: added http.js with base api links

---
+++ src/js/http/http.js
@@ -0,0 +1,3 @@
+export const BASE_API = "https://rickandmortyapi.com/api";
+export const CHARACTERS_API = "https://rickandmortyapi.com/api/character";
+export const EPISODES_API = "https://rickandmortyapi.com/api/episode";
