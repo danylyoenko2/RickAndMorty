71e6c897f29df3d8f359056c845617399890e181
Богдан <bohdansalo31@gmail.com>
2025-12-02 17:54:48 +0200

    створив секцію скролл та додав до неї функціонал

---
+++ src/js/components/scroll.js
@@ -0,0 +1,13 @@
+import Handlebars from "handlebars";
+import scrollSource from "bundle-text:../../template/scroll.hbs";
+import { RickAndMortyService } from "../service/RickAndMortyService";
+
+const scrollContainer = document.querySelector("[data-scroll]");
+const scrollTemp = Handlebars.compile(scrollSource);
+
+export const Scroll = async () => {
+  const data = await RickAndMortyService.getAllCharacters();
+  const scrollHTML = scrollTemp(data.results);
+  scrollContainer.innerHTML = scrollHTML;
+  console.log(scrollHTML);
+};
