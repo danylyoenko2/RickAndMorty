f9a9e86d7d2573c03170fad4c071ca6e0d303eeb
Danil <lockmaster19@gmail.com>
2025-12-04 05:04:54 +0200

    crct: corrected character cards function for easy using with pagination

--- src/js/components/CharacterCards.js
+++ src/js/components/CharacterCards.js
@@ -5,9 +5,19 @@ import charCardsSource from "bundle-text:../../template/characterCard.hbs";
 const charCardsTemp = Handlebars.compile(charCardsSource);
 const charCardsList = document.querySelector("[data-character-list]");
 
+let page = 0;
+let maxPage = 1;
+
 export const CharacterCards = async () => {
   if (location.pathname === "/") return;
-  const { results } = await RickAndMortyService.getAllCharacters();
-  const charCardsHTML = charCardsTemp(results);
-  charCardsList.insertAdjacentHTML("beforeend", charCardsHTML);
+  if (maxPage >= page) {
+    const { info, results } = await RickAndMortyService.getAllCharacters({
+      page: page,
+    });
+    maxPage = info.pages;
+
+    const charCardsHTML = charCardsTemp(results);
+    charCardsList.insertAdjacentHTML("beforeend", charCardsHTML);
+    page++;
+  }
 };
