dca8c007047fa4b914f691a42a68aea68cee45a3
mollys <162820512+danylyoenko2@users.noreply.github.com>
2025-12-05 20:19:06 +0200

    Uncomment scroll.js imports and Scroll function

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,13 +1,12 @@
-// import Handlebars from "handlebars";
-// import scrollSource from "bundle-text:../../template/scroll.hbs";
-// import { RickAndMortyService } from "../service/RickAndMortyService";
+import Handlebars from "handlebars";
+import scrollSource from "bundle-text:../../template/scroll.hbs";
+import { RickAndMortyService } from "../service/RickAndMortyService";
 
-// const scrollContainer = document.querySelector("[data-scroll]");
-// const scrollTemp = Handlebars.compile(scrollSource);
+const scrollContainer = document.querySelector("[data-scroll]");
+const scrollTemp = Handlebars.compile(scrollSource);
 
-// export const Scroll = async () => {
-//   const data = await RickAndMortyService.getAllCharacters();
-//   const scrollHTML = scrollTemp(data.results);
-//   scrollContainer.innerHTML = scrollHTML;
-//   console.log(scrollHTML);
-// };
+export const Scroll = async () => {
+  const data = await RickAndMortyService.getAllCharacters();
+  const scrollHTML = scrollTemp(data.results);
+  scrollContainer.innerHTML = scrollHTML;
+};
