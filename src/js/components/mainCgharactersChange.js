ec37a0177bfdab667396f8d5725da466150ff682
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-12-16 21:33:45 +0200

    fixed hero radius and changed a little mainCharacterchange

--- src/js/components/mainCgharactersChange.js
+++ src/js/components/mainCgharactersChange.js
@@ -1,11 +1,31 @@
+import mortySmith from "../../images/morty-smith.png";
+import summerSmith from "../../images/summer-smith.png";
+import bethSmith from "../../images/beth-smith.png";
+import jerrySmith from "../../images/jerry-smith.png";
+
 const characters = document.querySelectorAll(".main-character");
 const image = document.querySelector(".main-characters__img");
 const imageWrapper = document.querySelector(".main-characters__image");
 
+const characterImages = {
+  mortysmith: mortySmith,
+  summersmith: summerSmith,
+  bethsmith: bethSmith,
+  jerrysmith: jerrySmith,
+};
+
+console.log(bethSmith);
+
 characters.forEach((character) => {
   character.addEventListener("click", () => {
-    const newImage = character.dataset.image;
+    const characterPhotoMeta = character.dataset.image;
+    const newImage = characterImages[characterPhotoMeta];
+
+    console.log(characterImages[characterPhotoMeta]);
+
     const newBg = character.dataset.bg;
+
+    console.log(characterPhotoMeta);
 
     image.src = newImage;
     imageWrapper.style.backgroundColor = newBg;
