5d1f15a30d58d84ef49cf6b31be97b9fdb40d411
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-12-17 15:34:43 +0200

    fixed photo in main characters now it is change

--- src/js/components/mainCgharactersChange.js
+++ src/js/components/mainCgharactersChange.js
@@ -1,31 +1,20 @@
-import mortySmith from "../../images/morty-smith.png";
-import summerSmith from "../../images/summer-smith.png";
-import bethSmith from "../../images/beth-smith.png";
-import jerrySmith from "../../images/jerry-smith.png";
-
 const characters = document.querySelectorAll(".main-character");
 const image = document.querySelector(".main-characters__img");
 const imageWrapper = document.querySelector(".main-characters__image");
 
 const characterImages = {
-  mortysmith: mortySmith,
-  summersmith: summerSmith,
-  bethsmith: bethSmith,
-  jerrysmith: jerrySmith,
+  rick: new URL("../../images/rick2.webp", import.meta.url).href,
+  morty: new URL("../../images/morty-smith.png", import.meta.url).href,
+  summer: new URL("../../images/summer-smith.png", import.meta.url).href,
+  beth: new URL("../../images/beth-smith.png", import.meta.url).href,
+  jerry: new URL("../../images/jerry-smith.png", import.meta.url).href,
 };
-
-console.log(bethSmith);
 
 characters.forEach((character) => {
   character.addEventListener("click", () => {
-    const characterPhotoMeta = character.dataset.image;
-    const newImage = characterImages[characterPhotoMeta];
-
-    console.log(characterImages[characterPhotoMeta]);
-
+    const characterKey = character.dataset.image;
+    const newImage = characterImages[characterKey];
     const newBg = character.dataset.bg;
-
-    console.log(characterPhotoMeta);
 
     image.src = newImage;
     imageWrapper.style.backgroundColor = newBg;
